76562f403e8e43bc313c324fc92a7452360015c3
The Zux <thevupro753@gmail.com>
2025-01-09 14:05:38 +0700

    test

--- commit.bat
+++ commit.bat
@@ -1,36 +1,14 @@
 @echo off
-:: Display the list of branches
-echo Available branches:
-git branch
-echo.
+:: Nhắc nhập commit message
+set /p commitMessage=Nhập commit message: 
 
-:: Prompt the user to enter the branch name
-set /p branchName=Enter the branch name to push (default: current branch): 
-
-:: If no branch name is entered, use the current branch
-if "%branchName%"=="" (
-    for /f "tokens=2 delims=* " %%a in ('git branch ^| findstr "*"') do set branchName=%%a
-)
-
-:: Prompt the user to enter a commit message
-set /p commitMessage=Enter commit message: 
-
-:: Execute git commands
+:: Thực hiện các lệnh git
 git add .
 git commit -m "%commitMessage%"
+git push --set-upstream origin dev
 
-:: Check if the branch has an upstream
-git rev-parse --abbrev-ref --symbolic-full-name @{u} >nul 2>&1
-if errorlevel 1 (
-    echo No upstream branch set for "%branchName%".
-    echo Setting upstream branch...
-    git push --set-upstream origin %branchName%
-) else (
-    git push origin %branchName%
-)
-
-:: Display completion message
+:: Hiển thị thông báo hoàn tất
 echo ------------------------------------
-echo Code committed and pushed to branch "%branchName%" successfully!
+echo Commit và push code hoàn tất!
 echo ------------------------------------
 pause
